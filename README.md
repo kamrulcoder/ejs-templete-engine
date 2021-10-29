7d8c5c82435002223b9eebc2bd86657b2018d37c
kamrulcoder <kamrulcoder@gmail.com>
2021-10-29 16:15:30 -0700

    final project

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# ejs-templete-engine
+# Ejs Templete Engine 
+
+> ###  Whats is EJS? 
+> >
+ >  EJS হল একটি সাধারণ টেমপ্লেটিং ভাষা যা আপনাকে প্লেইন জাভাস্ক্রিপ্ট দিয়ে HTML মার্কআপ তৈরি করতে দেয়। কীভাবে জিনিসগুলিকে সংগঠিত করতে হয় সে সম্পর্কে কোনও ধর্মীয়তা নেই। পুনরাবৃত্তি এবং নিয়ন্ত্রণ-প্রবাহের কোন পুনঃউদ্ভাবন নেই। এটা শুধু প্লেইন জাভাস্ক্রিপ্ট.
+
+ <br/>
+
+
+> ## How to setup ejs?
+> 
+
+<br>
+
+#### Ejs Install Command 
+    npm install ejs
+
+#### Index js file require and setup
+
+```javascript
+// ejs set 
+    app.set("view engine", "ejs");
+
+// and new folder create name is views 
+```
+
+####  How to use  ejs templete for condtion 
+```javascript
+    <% if(post.published) { %>
+        <h3>
+            <%= post.title %>
+        </h3>
+        <p>
+            <%= post.body %>
+        </p>
+        <% } else{%>
+            <p>Not found Post </p>
+     <% } %>
+```
+
+####  How to use  ejs templete  for variable   
+```javascript 
+  <%= title %>
+```
+
--- index.js
+++ index.js
@@ -17,15 +17,34 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
+app.get("/about", (req, res)=> {
+    res.render("pages/about", {
+        title:"About Page "
+    })
+})
+
+app.get("/contact", (req, res)=> {
+    res.render("pages/contact", {
+        title:"Contact Page  "
+    })
+})
+
+
 app.get("/", (req, res)=> {
     let post =  {
         title:"Post Title ",
         body:" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas ratione culpa deserunt quibusdam inventore maiores cumque tempora, voluptate nobis delectus pariatur sapiente? Consequuntur magnam sapiente minima tempora dicta repellat id.",
         published:true
-
     }
-    res.render("index", {
-        title:"Ejs templete practice ",post
+
+    let posts = [
+        {title:"title  one ", author:"kamrul hasan "},
+        {title:"title  one ", author:"kamrul hasan "},
+        {title:"title  one ", author:"kamrul hasan "},
+        {title:"title  one ", author:"kamrul hasan "}
+    ]
+    res.render("pages/index", {
+        title:"Ejs templete practice ",post,posts
     });
 })
 
